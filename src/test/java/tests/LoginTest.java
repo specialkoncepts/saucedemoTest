d66fa876786dc844db4bd98982a2f18356724e14
mycupcake7 <mycupcake7@gmail.com>
2022-10-11 22:13:32 -0400

    chages from Chris

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -9,4 +9,11 @@ public class LoginTest {
 
 
     }
+
+    @Test(testName = "US 302 -  When locked out user tries to login with right password I would like to see an error"+
+            "message Epic sadface: Sorry, this user has been locked out")
+    public void test02()
+    {
+
+    }
 }
