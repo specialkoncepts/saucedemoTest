de111b2e405cd48a713bbda4271819ab4bfd86e2
specialkoncepts <kbakaush@gmail.com>
2022-10-11 20:57:56 -0400

    pom edits

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test(testName = "US 301 - Verify standard_user can login with right password")
+    public void test01() {
+
+
+    }
+}
