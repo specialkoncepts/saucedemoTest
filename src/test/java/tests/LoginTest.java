4a82f6362da950a65d88ce8404dbd015e1a2940b
specialkoncepts <kbakaush@gmail.com>
2022-10-13 19:28:39 -0400

    switch

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,24 +1,63 @@
 package tests;
 
+import pages.SaucedemoPage;
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.BrowserUtils;
 
-public class LoginTest extends BaseTest {
+public class SaucedemoTest extends BaseTest {
+
+    SaucedemoPage page;
+
+    @BeforeMethod (alwaysRun = true)
+    public void setup() {
+        driver.findElement(By.id("sauce-demo")).click();
+        BrowserUtils.switchToNewWindow(driver);
+        page = new SaucedemoPage(driver);
+    }
+    @AfterMethod (alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
 
     @Test(testName = "US 301 - Verify standard_user can login with right password")
     public void test01() {
 
+        // driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        page.userName.sendKeys("standard_user");
+        // driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        page.userPassword.sendKeys("secret_sauce");
 
-        driver.get("http://automation.techleadacademy.io/#/");
+        // driver.findElement(By.id("login-button")).click();
+        page.click(page.loginButton);
 
-        driver.findElement(By.id("sauce-demo")).click();
+        String expected = "PRODUCTS";
+        String actual = driver.findElement(By.xpath("//div[@class=\"header_secondary_container\"]/span")).getText();
 
+        Assert.assertEquals(actual, expected);
 
-        driver.findElement(By.id("user-name")).sendKeys();
+    }
+
+    @Test(testName = "US 302")
+    public void test02() {
+
+        page.userName.sendKeys("locked_out_user");
+
+        page.userPassword.sendKeys("secret_sauce");
+
+        page.click(page.loginButton);
+
+        //System.out.println(driver.findElement(By.xpath("//h3")).getText());
+
+        String expected = "Epic sadface: Sorry, this user has been locked out.";
+        String actual = driver.findElement(By.xpath("//h3")).getText();
+
+        Assert.assertEquals(expected, actual);
+
 
 
 
