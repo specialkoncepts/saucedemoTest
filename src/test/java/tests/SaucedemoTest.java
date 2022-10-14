0f1680b4e4c26383d735c83cb3ff8cf22a35bf88
unknown <achimtoum@yahoo.com>
2022-10-13 20:46:57 -0400

    added testcase 305, 306

--- src/test/java/tests/SaucedemoTest.java
+++ src/test/java/tests/SaucedemoTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import data.DataProviders;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import pages.SaucedemoPage;
 import base.BaseTest;
 import org.openqa.selenium.By;
@@ -8,6 +11,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.BrowserUtils;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SaucedemoTest extends BaseTest {
 
@@ -21,15 +27,15 @@ public class SaucedemoTest extends BaseTest {
     }
     @AfterMethod (alwaysRun = true)
     public void tearDown() {
-        driver.quit();
+    //    driver.quit();
     }
 
     @Test(testName = "US 301 - Verify standard_user can login with right password")
     public void test01() {
 
-        // driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
         page.userName.sendKeys("standard_user");
-        // driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
         page.userPassword.sendKeys("secret_sauce");
 
         // driver.findElement(By.id("login-button")).click();
@@ -58,8 +64,69 @@ public class SaucedemoTest extends BaseTest {
 
         Assert.assertEquals(expected, actual);
 
+    }
+
+    @Test(testName = "US 304 - I need an option to see navigation menu. And confirm all buttons are present")
+    public void test04() {
 
 
+//        driver.get("http://automation.techleadacademy.io/#/");
+//
+//        driver.findElement(By.id("sauce-demo")).click();
+//        BrowserUtils.switchToNewWindow(driver);
+
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        System.out.println(driver.findElement(By.xpath("//a[@id=\"inventory_sidebar_link\"]")).getText());
+
+
+//        Assert.assertEquals(driver.findElement(By.id("inventory_sidebar_link")).getText(), "All Items");
+//        Assert.assertEquals(driver.findElement(By.id("about_sidebar_link")).getText(), "About");
+//        Assert.assertEquals(driver.findElement(By.id("logout_sidebar_link")).getText(), "Logout");
+//        Assert.assertEquals(driver.findElement(By.id("reset_sidebar_link")).getText(), "Reset App State");
+
+    }
+
+    @Test(testName = "US 305 - Footer of the page should be © 2022 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy")
+    public void test05() {
+
+        page.userName.sendKeys("standard_user");
+
+        page.userPassword.sendKeys("secret_sauce");
+
+        page.click(page.loginButton);
+
+        String expected = "© 2022 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy";
+        String actual = driver.findElement(By.xpath("//div[@class='footer_copy']")).getText();
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test(testName = "US 306 - Filter options. When user clicks the filter it should have following options:")
+    public void test06() {
+
+        page.userName.sendKeys("standard_user");
+
+        page.userPassword.sendKeys("secret_sauce");
+
+        page.click(page.loginButton);
+        String [] btn = new String[] {"az", "za", "lohi", "hilo"};
+
+        for (int i = 0; i < 4 ; i++) {
+            String actual = driver.findElement(By.xpath("//select/option["+ (i+1) + "]")).getAttribute("value");
+            String expected = btn[i];
+            Assert.assertEquals(actual, expected);
+        }
 
     }
 }
